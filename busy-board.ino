b327036b4c1eba35890ee29e29a0a914ab58b543
Adam Östör <adam.ostor@thirdbridge.com>
2018-08-12 17:24:40 +0100

    Add fading out effect when button is pressed

--- busy-board.ino
+++ busy-board.ino
@@ -6,52 +6,74 @@ Busy Board
 #define GREEN_LED 3
 #define AMBER_BTN 4
 #define AMBER_LED 5
-#define RED_BTN   6
-#define RED_LED   7
+#define RED_BTN   7 // due to the PWM availability the order is swapped for red
+#define RED_LED   6
 #define BLUE_BTN  8
 #define BLUE_LED  9
 #define WHITE_BTN 10
 #define WHITE_LED 11
+#define DARKEST 0
+#define BRIGHTEST 255
+
+int greenBrightness = DARKEST;
+int amberBrightness = DARKEST;
+int redBrightness   = DARKEST;
+int blueBrightness  = DARKEST;
+int whiteBrightness = DARKEST;
+
+int const fadeAmount = 5;
 
 void setup() {
   pinMode(GREEN_BTN, INPUT_PULLUP);
   pinMode(GREEN_LED, OUTPUT);
-  digitalWrite(GREEN_LED, LOW);
+  analogWrite(GREEN_LED, greenBrightness);
 
   pinMode(AMBER_BTN, INPUT_PULLUP);
   pinMode(AMBER_LED, OUTPUT);
-  digitalWrite(AMBER_LED, LOW);
+  analogWrite(AMBER_LED, amberBrightness);
 
   pinMode(RED_BTN, INPUT_PULLUP);
   pinMode(RED_LED, OUTPUT);
-  digitalWrite(RED_LED, LOW);
+  analogWrite(RED_LED, redBrightness);
 
   pinMode(BLUE_BTN, INPUT_PULLUP);
   pinMode(BLUE_LED, OUTPUT);
-  digitalWrite(BLUE_LED, LOW);
+  analogWrite(BLUE_LED, blueBrightness);
 
   pinMode(WHITE_BTN, INPUT_PULLUP);
   pinMode(WHITE_LED, OUTPUT);
-  digitalWrite(WHITE_LED, LOW);
+  analogWrite(WHITE_LED, whiteBrightness);
 }
 
 void loop() {
-  handleArcadeButton(GREEN_BTN, GREEN_LED);
-  handleArcadeButton(AMBER_BTN, AMBER_LED);
-  handleArcadeButton(RED_BTN,   RED_LED);
-  handleArcadeButton(BLUE_BTN,  BLUE_LED);
-  handleArcadeButton(WHITE_BTN, WHITE_LED);
+  greenBrightness = handleArcadeButton(GREEN_BTN, GREEN_LED, greenBrightness);
+  amberBrightness = handleArcadeButton(AMBER_BTN, AMBER_LED, amberBrightness);
+  redBrightness   = handleArcadeButton(RED_BTN,   RED_LED,   redBrightness);
+  blueBrightness  = handleArcadeButton(BLUE_BTN,  BLUE_LED,  blueBrightness);
+  whiteBrightness = handleArcadeButton(WHITE_BTN, WHITE_LED, whiteBrightness);
+  delay(50);
 }
 
 bool btnIsPressed(int btn) {
   return HIGH != digitalRead(btn);
 }
 
-void handleArcadeButton(int btn, int led) {
+int handleArcadeButton(int btn, int led, int brightness) {
   if (btnIsPressed(btn)) {
-    digitalWrite(led, HIGH);
-  } else {
-    digitalWrite(led, LOW);
+    analogWrite(led, BRIGHTEST);
+
+    return BRIGHTEST;
   }
+  analogWrite(led, brightness);
+
+  return fade(brightness);
 }
 
+int fade(int brightness) {
+  int dimmed = brightness - fadeAmount;
+  if (dimmed > DARKEST) {
+    return dimmed;
+  }
+
+  return DARKEST;
+}
